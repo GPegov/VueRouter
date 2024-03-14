9b20cd0a8e5bbdbb85976f782aa55c57f70488b2
GPegov <66030738+GPegov@users.noreply.github.com>
2024-03-15 00:17:24 +0500

    I've added menu buttons in the navbar and started making a dropdown menu

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,9 +1,20 @@
 <template>
 
-    <div id="nav">
-      <div class="navItem">
-        <router-link id="logo" to="/">Виталий и Екатерина</router-link>
-      </div>
+    <nav id="nav">
+      <transition name="fade" mode="out-in">
+        <div class="navItem menuBar">
+          
+          <img id="menuIcon" v-show ="show === false" src="@/img/MenuIcons/MenuIcon.png" 
+            alt="Menu" @click="show = !show" :key="Date.now()"/>
+
+          <img id="clear" v-show="show === true" src="@/img/MenuIcons/Clear.png" 
+            alt="Clear" @click="show = !show" :key="Date.now()"/>
+            
+      
+          <router-link id="logo" to="/">Виталий и Екатерина</router-link>
+        </div>
+      </transition>
+      
       <div class="navItem">
         <router-link to="/CheckIn">Подтвердите присутствие</router-link>
       </div>
@@ -17,23 +28,51 @@
         <router-link to="/SeatingPlan">План рассадки гостей</router-link>
       </div>
       
-  </div>
+    </nav>
 </template>
 
-<script>
-
+<script setup>
+import { ref } from 'vue'
+const show = ref(false);
 </script>
 
 <style>
 #nav .active-link {
     color: rgb(130, 255, 255);
 }
+#menuIcon {
+  display: none;
+}
 
 @media screen and (max-width: 500px) {
+#menuIcon {
+  display: flex;
+  width: 60px;
+  height: 60px;
+  margin-bottom: 10px;
+  margin-right: 70px;
+  margin-left: -15px;
+  cursor:pointer;
+  
+}
+#clear {
+  display: flex;
+  width: 60px;
+  height: 60px;
+  margin-bottom: 10px;
+  margin-right: 70px;
+  margin-left: -15px;
+  cursor:pointer;
+}
 .navItem {
   display: flex;
   flex-direction: column;
-  padding-top: 20px;
+  padding-top: 10px;
+}
+.menuBar{
+  display: flex;
+  flex-direction: row;
+
 }
 #nav {
   display: flex;
